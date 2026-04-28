d50ff56dd9a91a371846a3d6b3a5c28adda29c85
gomathy <gomathy@propertyfinder.ae>
2026-04-28 12:05:49 +0400

    feat: add homepage test cases

---
+++ src/test/java/tests/HomepageTest.java
@@ -0,0 +1,32 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utils.DriverManager;
+
+public class HomepageTest extends BaseTest {
+
+    @Test
+    public void testHomepageTitle() {
+        DriverManager.getDriver().get("https://automationexercise.com/");
+        
+        String actualTitle = DriverManager.getDriver().getTitle();
+        Assert.assertTrue(actualTitle.contains("Automation Exercise"), 
+            "Homepage title should contain 'Automation Exercise'");
+        
+        System.out.println("Homepage title verified: " + actualTitle);
+    }
+
+    @Test
+    public void testHomepageLogoDisplayed() {
+        DriverManager.getDriver().get("https://automationexercise.com/");
+        
+        boolean isLogoDisplayed = DriverManager.getDriver()
+            .findElement(org.openqa.selenium.By.cssSelector("img[alt='Website for automation practice']"))
+            .isDisplayed();
+        
+        Assert.assertTrue(isLogoDisplayed, "Logo should be displayed on homepage");
+        
+        System.out.println("Homepage logo is displayed!");
+    }
+}
